4abe19468b433bdc57a9c726f370454d321e2a4d
Shubham Saurav <ssaurav28502@gmail.com>
2024-07-06 15:54:34 +0530

    style change

--- frontend/src/Components/pages/AllVlogs.js
+++ frontend/src/Components/pages/AllVlogs.js
@@ -56,41 +56,26 @@ function AllVlogs() {
 
     return (
         <>
-
-
-
-
-                             <Navbar />
-
-               
-
+            <Navbar />
             <div className="mt-24 mb-3 md:w-96 mx-auto">
-                            <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                            <h2 className='max-w-md mx-5 mx-auto text-4xl z-51 m-4 font-semibold'>Blogs</h2>
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                            <h2 className='max-w-md mx-auto text-4xl z-51 m-4 font-semibold'>Blogs</h2>
                                 <input
                                 value={searchtxt}
+                                onChange={(e)=>{setSeachTxt(e.target.value);
 
-            onChange={(e)=>{
-
-            setSeachTxt(e.target.value);
-
-
-            }
-            } 
-                                    type="search"
-                        className=" relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                        placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="button-addon3" />
+                                }} 
+                                type="search"
+                                className=" relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                                placeholder="Search"
+                                aria-label="Search"
+                                aria-describedby="button-addon3" />
 
                     {/* <!--Search button--> */}
                     
                     <button
                     onClick={()=>{
-                // need to filter data
-                
-            const data =  filterData(searchtxt,vlogs);
-                // update the state = filtervlogs variable
+                const data =  filterData(searchtxt,vlogs);
                 setFilterVlogs(data);
             }}
                    className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
