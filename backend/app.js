cd26bf0580ff10eda2cae1ea5deac7e44fb24c1e
MANAV MITTAL <manavmittal2144@gmail.com>
2024-07-06 16:15:40 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose')
-const methodOverride = require('method-override');
-const userRoutes = require('./Routes/auth');
-const vlogRoutes = require('./Routes/vlog');
-const cardRoutes = require('./Routes/card');
-const cors = require('cors');
-const seedDB = require('./seed')
-
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const userRoutes = require("./Routes/auth");
+const vlogRoutes = require("./Routes/vlog");
+const cardRoutes = require("./Routes/card");
+const cors = require("cors");
+const seedDB = require("./seed");
 
 const router = express.Router();
-const User = require('./Models/User');
-const bcrypt = require('bcrypt');
+const User = require("./Models/User");
+const bcrypt = require("bcrypt");
 const salt = bcrypt.genSaltSync(10);
 
-mongoose.connect('mongodb://127.0.0.1:27017/vlog_web')
-.then(() => {
-    console.log("db successfully connected")
-})
-.catch((err) => {
+mongoose
+  .connect("mongodb+srv://ommittal2144:ommittal@cluster0.jvpqfkn.mongodb.net/")
+  .then(() => {
+    console.log("db successfully connected");
+  })
+  .catch((err) => {
     console.log("db not connected");
     console.log(err);
-});
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// seedDB();
+//seedDB();
 app.use(cardRoutes);
 app.use(userRoutes);
 app.use(vlogRoutes);
 app.listen(8080, () => {
-    console.log('connected')
-})
+  console.log("connected");
+});
